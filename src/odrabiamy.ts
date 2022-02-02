6a5a2b7a55a2489eaec59547212f3fde4b6d6905
Michsl <69900794+Michsl@users.noreply.github.com>
2022-02-02 17:39:37 +0100

    fixed rendering issues: double math equations and screesnhot being realy wide

--- src/odrabiamy.ts
+++ src/odrabiamy.ts
@@ -19,12 +19,18 @@ export default function getExerciseImage(exerciseDetails: ExerciseDetails, autho
 
         const browser = await puppeteer.launch({timeout: 100000});
         const page = await browser.newPage();
-        await page.setViewport({width: 1920, height: 1});
-        console.log(decodeURI(solution))
-        await page.setContent(decodeURI(solution), {waitUntil: 'networkidle0'}); // zamiast waitfortimeout
+        await page.setViewport({width: 780, height: 1});
+        let decoded_solution = decodeURI(solution)
+        // decoded_solution = decoded_solution.replaceAll('<hr>', '')
+        decoded_solution = decoded_solution.replaceAll(/<object class="math small".*?>/g, '')
+        console.log(decoded_solution)
+        const loaded = page.waitForNavigation({waitUntil: 'load'});
+        await page.setContent(decoded_solution, {waitUntil: 'networkidle0'});
+        await loaded // zamiast waitfortimeout
         //await page.waitForTimeout(1000)
         const bodyHeight = await page.evaluate(() => document.body.scrollHeight);
-        await page.setViewport({width: 1920, height: bodyHeight});
+        const bodyWidth = await page.evaluate(() => document.body.scrollWidth);
+        await page.setViewport({width: bodyWidth, height: bodyHeight});
         const screenshot = await page.screenshot({fullPage: true})
 
         if (Buffer.isBuffer(screenshot)) {
